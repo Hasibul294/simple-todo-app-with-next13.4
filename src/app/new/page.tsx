5c4ed930723d8b2f7c7d59db1fb34aed9c2bc370
hasib <hasib.shikkha@gmail.com>
2023-05-25 19:10:07 +0600

    add experimental server actions

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,21 @@
 import Link from 'next/link';
 import React from 'react'
+import async from '../page';
+import { prisma } from '@/db';
+import { redirect } from 'next/navigation';
+
+async function createTodo(data: FormData){
+  "use server";
+
+  const title = data.get('title')?.valueOf();
+  if(typeof title !== 'string' || title.length === 0){
+    throw new Error('Invalid title');
+  }
+
+  await prisma.todo.create({data: {title, complete:false}})
+  redirect('/')
+
+};
 
 const NewPage = () => {
   return (
@@ -7,14 +23,14 @@ const NewPage = () => {
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">New</h1>
       </header>
-      <form className="flex gap-2 flex-col" action="">
+      <form className="flex gap-2 flex-col" action={createTodo}>
         <input
           type="text"
           name="title"
           className="border border-slate-300 bg-transparent rounded
           px-2 py-1 outline-none focus-within:border-slate-100"
         />
-        <div className='flex gap-1 justify-end'>
+        <div className="flex gap-1 justify-end">
           <Link
             href=".."
             className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
