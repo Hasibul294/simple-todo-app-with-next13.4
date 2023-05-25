661ba0a6a988a891c41097353afa6a4a298aabd5
hasib <hasib.shikkha@gmail.com>
2023-05-25 17:59:04 +0600

    add new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { prisma } from '@/db'
 import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Home() {
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany()
+  // await prisma.todo.create({ data: { title: "test", complete: false } });
+
+
   return (
     <main >
-      <h1>Hi</h1>
+      <header className='flex justify-between items-center mb-4'>
+        <h1 className='text-2xl'>Todos</h1>
+        <Link className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none' href='new'>New</Link>
+      </header>
+      <ul>
+        {
+          todos.map(todo => <li key={todo.id}>{todo.title}</li>)
+        }
+      </ul>
     </main>
   )
 }
